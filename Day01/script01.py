468860431c4dc252412377e9599f56b62161e807
HashanEranga <ccmrhe@gmail.com>
2022-12-28 18:56:47 +0530

    added script01

---
+++ Day01/script01.py
@@ -0,0 +1,15 @@
+# basic print command
+print("Hello World")
+
+# single line comment
+""" multiple line comments """
+# Terminal commands 
+
+"""
+Terminal commands
+ls - view files 
+cd - change directory
+python <filename.py> - run a python file named <filename.py>
+"""
+
+print("Hello I'm Hashan Eranga")
